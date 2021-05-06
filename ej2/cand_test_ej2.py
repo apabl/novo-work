1bc000a34c9210e9b3b269e1e9ec15e761173354
Pablo Abou Adal <>
2021-05-06 20:49:39 -0300

    Write 'expected_ej2.v' file

--- ej2/cand_test_ej2.py
+++ ej2/cand_test_ej2.py
@@ -1,10 +1,16 @@
 import re
 
-with open('testcase.v') as f:
-    content = f.read()
+def run():
+    with open('testcase.v') as f:
+        content = f.read()
 
-partdata = re.search(r'  reg \[(.*)\] (\S*) \[(.*)\];\n  initial begin\n((    \S*\[\S*\] = \S*;\n)*)  end\n',
-                     content)
+    partdata = re.search(r'^((.*\n)+)(  initial begin\n)((    \S*\[\S*\] = \S*;\n)*)(  end\n)((.*\n)+)$',
+                        content)
 
-for g in range(5):
-    print(partdata.group(g))
+    with open('expected_ej2.v', 'w') as file:
+        file.write(partdata.group(1))
+        file.write('$readmemh("memdump0.mem", mem);\n')
+        file.write(partdata.group(7))
+
+if __name__ == "__main__":
+    run()

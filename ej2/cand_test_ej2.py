dd9d58cbc8920a886a1bc4ac63d199b780eba231
Pablo Abou Adal <>
2021-05-06 21:45:30 -0300

    Write 'memdump0.mem'

--- ej2/cand_test_ej2.py
+++ ej2/cand_test_ej2.py
@@ -5,12 +5,17 @@ def run():
         content = f.read()
 
     partdata = re.search(r'^((.*\n)+)(  initial begin\n)((    \S*\[\S*\] = \S*;\n)*)(  end\n)((.*\n)+)$',
-                        content)
+                         content)
 
-    with open('expected_ej2.v', 'w') as file:
-        file.write(partdata.group(1))
-        file.write('$readmemh("memdump0.mem", mem);\n')
-        file.write(partdata.group(7))
+    with open('expected_ej2.v', 'w') as file_expected:
+        file_expected.write(partdata.group(1))
+        file_expected.write('  $readmemh("memdump0.mem", mem);\n')
+        file_expected.write(partdata.group(7))
+
+    with open('memdump0.mem', 'w') as file_memdump:
+        
+        for line in partdata.group(4).splitlines():
+            file_memdump.write(f"{line[-3:-1]}\n")
 
 if __name__ == "__main__":
     run()
